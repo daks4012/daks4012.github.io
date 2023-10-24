dde1880285eddf4dc7e612be8de365316727ccbf
Daks(jongsu Baek) <51820806+daks4012@users.noreply.github.com>
2023-10-24 14:32:43 +0900

    Add files via upload

--- REACT/shop/dummy/newPerfume.js
+++ REACT/shop/dummy/newPerfume.js
@@ -1,30 +1,30 @@
 [
     {
-        id: 6,
-        title: "kiwi extract",
-        content: "Born in kiwi",
-        price: 190,
-        writer: "Mareefe"
+        "id": 6,
+        "title": "kiwi extract",
+        "content": "Born in kiwi",
+        "price": 190,
+        "writer": "Mareefe"
     },
     {
-        id: 7,
-        title: "soap refreshing",
-        content: "Born in refreshing",
-        price: 100,
-        writer: "Sponchia"
+        "id": 7,
+        "title": "soap refreshing",
+        "content": "Born in refreshing",
+        "price": 100,
+        "writer": "Sponchia"
     },
     {
-        id: 8,
-        title: "calendula",
-        content: "Born in calendula",
-        price: 105,
-        writer: "silviarita"
+        "id": 8,
+        "title": "calendula",
+        "content": "Born in calendula",
+        "price": 105,
+        "writer": "silviarita"
     },
     {
-        id: 9,
-        title: "Primal aromatherapy",
-        content: "Born in aromatherapy",
-        price: 145,
-        writer: "kerdkanno"
+        "id": 9,
+        "title": "Primal aromatherapy",
+        "content": "Born in aromatherapy",
+        "price": 145,
+        "writer": "kerdkanno"
     }
 ]
